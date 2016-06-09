44427fd08ba8b0f0b8fb5c74d78943fd7f7dc70d
AbNadi <abnadi@me.com>
2016-06-09 05:10:10 -0700

    0004

--- tacta-sn.rb
+++ tacta-sn.rb
@@ -1,3 +1,5 @@
+#CONTROLLER
+
 require 'sinatra'
 require './contacts_file'
 
@@ -7,8 +9,22 @@ get '/' do
    "<h1>Tacta Contact Manager</h1>"
 end
 
+#show all contacts
 get '/contacts' do
    @contacts = read_contacts
    erb :'contacts/index'
+end
+
+#New Contacts
+get '/contacts/new' do
+   erb :'contacts/new'
+end
+
+#show individual contacts
+get '/contacts/:i' do
+   @i = params[:i].to_i
+   contacts = read_contacts
+   @contact = contacts[@i]
+   erb :'contacts/show'
 end
 
