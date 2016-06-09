79d5d81f5c104697aad79f95714b387eaca28cb9
AbNadi <abnadi@me.com>
2016-06-09 01:45:37 -0700

    0001

--- tacta.rb
+++ tacta.rb
@@ -6,24 +6,31 @@ def index(contacts)
   end
 end
 
-def show(contact)
-  puts
-  puts "#{contact[:name]}"
-  puts "phone: #{contact[:phone]}"
-  puts "email: #{contact[:email]}"
+def action_new( contacts )
+   contact = create_new
+
+   contacts << contact
+
+   puts
+   puts "New contact created:"
+   puts
+
+   show( contact )
+   puts
 end
 
-def create_new
-  contact = {}
+def action_show( contacts, i )
+   contact = contacts[i-1]
 
-  puts
-  puts "Enter contact info:"
+   puts
+   show( contact )
+   puts
+end
 
-  contact[:name ] = ask "Name? "
-  contact[:phone] = ask "Phone? "
-  contact[:email] = ask "Email? "
-
-  contact
+def show(contact)
+   puts "#{contact[:name]}"
+   puts "phone: #{contact[:phone]}"
+   puts "email: #{contact[:email]}"
 end
 
 def ask(prompt)
@@ -45,27 +52,12 @@ loop do
 
    puts
    response = ask "Who would you like to see (n for new, q to quit)? "
+
    break if response == "q"
 
    if response == "n"
-
-      contact = create_new
-
-      contacts << contact
-
-      puts
-      puts "New contact created:"
-      puts
-
-      show( contact )
-      puts
+      action_new( contacts )
    else
-      i = response.to_i
-
-      contact = contacts[i-1]
-
-      puts
-      show( contact )
-      puts
+      action_show( contacts, response.to_i )
    end
 end
